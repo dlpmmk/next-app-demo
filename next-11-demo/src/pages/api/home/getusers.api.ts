41728660899df24fc9ff4085103ee629bb33fb4e
dlpmmk <513393989@qq.com>
2023-01-07 06:20:18 +0000

    添加postgres数据库支持

--- next-11-demo/src/pages/api/home/getusers.api.ts
+++ next-11-demo/src/pages/api/home/getusers.api.ts
@@ -2,6 +2,7 @@ import { PageConfig } from 'next';
 import anylogger from 'anylogger';
 import '@mmstudio/an000042';
 import an48 from '@mmstudio/an000048';
+import tbuser from '../../../db/table/user';
 
 const logger = anylogger('pages/api/home/getusers.api');
 
@@ -25,14 +26,18 @@ export type Message = {
  */
 const handler = an48<Result>();
 
-handler.get(async(req, res) => {
+handler.get(async (req, res) => {
 	try {
 		logger.debug('msg body:', req.query);
 		const msg = req.query as Message;
 
+		// 获取所有用户
+		const userdata = await tbuser().query();
+		logger.debug("users:", userdata)
+
 		res.status(200).json({
 			ok: true,
-			data:{}
+			data: {}
 		});
 	} catch (error) {
 		logger.error(error);
